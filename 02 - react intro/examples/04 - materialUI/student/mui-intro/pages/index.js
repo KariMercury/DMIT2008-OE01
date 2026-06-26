8bccbf050b4170949a2f4444aba26ae16228cf39
KariMercury <kintherapy.kari@gmail.com>
2026-06-25 18:36:34 -0600

    added box, typography and layed with styles and colour

--- 02 - react intro/examples/04 - materialUI/student/mui-intro/pages/index.js
+++ 02 - react intro/examples/04 - materialUI/student/mui-intro/pages/index.js
@@ -3,6 +3,9 @@ import styles from "@/styles/Home.module.css";
 
 // MUI components
 import Container from '@mui/material/Container';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+
 
 export default function Home() {
   return (
@@ -15,8 +18,15 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <Container maxWidth="sm" style={{ background: '#cccccc'}}> 
-          some content we'll add later
+       <Container maxWidth="md" sx={{backgroundColor: '#cccccc'}}>
+          <Box sx={{ my: 4 }}>
+           <Typography variant="h2" component="h2">
+              MUI looks good.
+            </Typography>
+            <Typography variant="p" component="p">
+              You can perhaps see why this is a popular package.
+            </Typography>         
+          </Box>
         </Container>
       </main>
     </>
