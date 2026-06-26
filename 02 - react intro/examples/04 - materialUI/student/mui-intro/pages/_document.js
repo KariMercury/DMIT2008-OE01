bfd3261558b7cf9dab1019b6d0380516d37b80b8
KariMercury <kintherapy.kari@gmail.com>
2026-06-25 18:11:19 -0600

    practicing with container

--- 02 - react intro/examples/04 - materialUI/student/mui-intro/pages/_document.js
+++ 02 - react intro/examples/04 - materialUI/student/mui-intro/pages/_document.js
@@ -1,10 +1,14 @@
 import { Html, Head, Main, NextScript } from "next/document";
 
+// _document.js - applies to EVERY PAGE automatically
+//    - Head / Body / Footer / Typography? - styling ?
+
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
       <body>
+        put some stuff here
         <Main />
         <NextScript />
       </body>
