78aad9c0e9953d232bf3dfedf5a1240bfb67affb
KariMercury <kintherapy.kari@gmail.com>
2026-06-25 18:26:18 -0600

    add background colour to contianter stylle

--- 02 - react intro/examples/04 - materialUI/student/mui-intro/pages/_document.js
+++ 02 - react intro/examples/04 - materialUI/student/mui-intro/pages/_document.js
@@ -8,7 +8,7 @@ export default function Document() {
     <Html lang="en">
       <Head />
       <body>
-        put some stuff here
+        
         <Main />
         <NextScript />
       </body>
--- 02 - react intro/examples/04 - materialUI/student/mui-intro/pages/index.js
+++ 02 - react intro/examples/04 - materialUI/student/mui-intro/pages/index.js
@@ -13,8 +13,9 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+
       <main className={styles.main}>
-        <Container maxWidth="lg">
+        <Container maxWidth="sm" style={{ background: '#cccccc'}}> 
           some content we'll add later
         </Container>
       </main>
